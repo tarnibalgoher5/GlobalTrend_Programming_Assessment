d1052180d4dee715737d98c2e9b2510045e4a1a0
Tarni Balgoher <tarnibalgoher@gmail.com>
2024-07-16 20:14:51 +0530

    filter numbers

---
+++ Q10.js
@@ -0,0 +1,11 @@
+function capitalize(str){
+    //using the javascript split function to split the words of a sentence 
+    const eachWord = str.split('');
+    const capitalizedSentence = eachWord.map(eachWord => eachWord.charAt(0).toUpperCase() + eachWord.slice(1));
+
+    return capitalizedSentence.join('');
+}
+
+str = "hello! welcome to the global trend";
+const ans = capitalize(str);
+console.log(ans);
--- Q9.js
+++ Q9.js
@@ -1,1 +1,17 @@
 //Write a function that filters out even numbers from an array.
+function filterNumbers(arr){
+    let size = arr.length;
+    let odd = []; 
+    for(let i=0; i<size; i++){
+        if(arr[i] % 2 != 0){
+            odd.push(arr[i]);
+        }
+    }
+    return odd;
+}
+
+let arr = [2,0,3,5,7,8,9,6];
+let ans = filterNumbers(arr);
+for(let i=0; i<ans.length; i++){
+    console.log(ans[i]);
+}
