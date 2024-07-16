53fa78b2d29d7baf8cd94e89e90f72b343116117
Tarni Balgoher <tarnibalgoher@gmail.com>
2024-07-16 21:37:29 +0530

    string expression

--- Q2.js
+++ Q2.js
@@ -2,4 +2,30 @@
 //a simple arithmetic expression (only addition and subtraction) 
 //and returns the result.
 
+function calculate(expr) {
+    const tokens = expr.split(/(\+|-)/); // splitting the operators and numbers 
+
+    let result = parseFloat(tokens[0]);
+    for (let i =1; i<tokens.length; i += 2) {
+        const operator = tokens[i];
+        const value = parseFloat(tokens[i + 1]);
+        if (operator === "+"){
+            result += value;
+        }
+        else if (operator === "-"){
+            result -= value;
+        } 
+        else {
+            console.log("ENTER A VALID OPERATOR");
+        }
+    }
+    return result;
+  }
+
+  let expr1 = "2+9-3";
+  let expr2 = "6-1+4";
+  
+  console.log(calculate(expr1)); 
+  console.log(calculate(expr2));
+  
 
